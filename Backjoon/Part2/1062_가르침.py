a5ab5ac05ab992276b9f9400512bf5d0acc7a363
정종민 <whdals6831@gmail.com>
2021-03-17 22:47:20 +0900

    add level 1

---
+++ Backjoon/Part2/1062_가르침.py
@@ -0,0 +1,9 @@
+# 완전탐색의 유연한 생각
+n, k = map(int, input().split())
+
+['a', 'n', 't', 'i', 'c']
+
+words = []
+
+for _ in range(n):
+    words.append(input())
---
+++ Programmers/1_두_정수_사이의_합.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    if a > b:
+        a, b = b, a
+
+    sum = 0
+
+    for i in range(a, b+1):
+        sum += i
+
+    return sum
