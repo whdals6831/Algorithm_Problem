c943a24dcb576378bf753983d41703a398541a75
정종민 <whdals6831@gmail.com>
2021-03-18 23:51:00 +0900

    add level 1

--- Backjoon/Part2/1062_가르침.py
+++ Backjoon/Part2/1062_가르침.py
@@ -1,9 +1,25 @@
 # 완전탐색의 유연한 생각
-n, k = map(int, input().split())
+import sys
+from itertools import combinations
 
-['a', 'n', 't', 'i', 'c']
+n, k = map(int, sys.stdin.readline().split())
 
+alphabet = [[0]*26 for i in range(n)]
+ans = 0
+
+antatica = ['a', 'n', 'c', 't', 'i']
+
+teach_count = k - 5
 words = []
 
 for _ in range(n):
-    words.append(input())
+    words.append(sys.stdin.readline().rstrip()[4:-4])
+
+for i, v in enumerate(words):
+    for j in set(v):
+        if j not in antatica:
+            alphabet[i][ord(j)-97] += 1
+
+for i in alphabet:
+    print(i)
+
