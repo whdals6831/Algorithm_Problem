1210b512618a214eee14c119f36290380a883820
whdals6831 <whdals6831@gmail.com>
2021-03-30 03:10:33 +0900

    update 1916

--- Backjoon/Part2/1916_최소비용_구하기.py
+++ Backjoon/Part2/1916_최소비용_구하기.py
@@ -1,45 +1,37 @@
 import sys
-import math
+from heapq import heappush, heappop
 
 n = int(input())
 m = int(input())
 
-city = [[float('inf')]*(n+1) for _ in range(n+1)]
-visit = [0 for _ in range(n+1)]
+city = [[] for _ in range(n+1)]
+value = [float('inf') for _ in range(n+1)]
 
 for _ in range(m):
-    start, end, cost = map(int, sys.stdin.readline().split())
-    if math.isinf(city[start][end]):
-        city[start][end] = cost
-    else:
-        city[start][end] = min(city[start][end], cost)
+    start, end, weight = map(int, sys.stdin.readline().split())
+    city[start].append([end, weight])
 
 start_city, end_city = map(int, input().split())
 
-n = start_city
-city[n][n] = 0
+def dijkstra(start_city):
+    # 내장 모듈인 heapq는 최소 힙으로 만들어짐
+    # 그래서 pop을 하면 최소값이 반환됨
+    value[start_city] = 0
+    heap = []
+    heappush(heap, [0, start_city])
 
-while True:
-    visit[n] = 1
-    now = n
-    isNext = False
-    min_cost = float('inf')
+    while heap:
+        now_value, now_edge = heappop(heap)
 
-    for idx, cost in enumerate(city[n]):
-        city[idx][idx] = min(city[now][now]+cost, city[idx][idx])
-        if city[idx][idx] < min_cost and visit[idx] != 1:
-            isNext = True
-            n = idx
-            min_cost = city[idx][idx]
-    
-    if not isNext:
-        if 0 in visit[1:]:
-            for i in visit[1:]:
-                if visit[i] == 0:
-                    if min_cost > city[i][i]:
-                        min_cost = city[i][i]
-                        n = i
-        else:
-            break
-    
-print(city[end_city][end_city])
+        if now_value > value[now_edge]:
+            continue
+
+        for edge, weight in city[now_edge]:
+            new_weight = now_value + weight
+
+            if new_weight < value[edge]:
+                value[edge] = new_weight
+                heappush(heap, [new_weight, edge])
+
+dijkstra(start_city)
+print(value[end_city])
