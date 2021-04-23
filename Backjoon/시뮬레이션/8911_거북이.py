a0fd03d3056a7444636290572259d008ed60ca51
whdals6831 <whdals6831@gmail.com>
2021-04-23 23:01:48 +0900

    add 8911

---
+++ Backjoon/시뮬레이션/8911_거북이.py
@@ -0,0 +1,94 @@
+from sys import stdin
+
+t = int(input())
+commands = [stdin.readline().rstrip() for _ in range(t)]
+answer = []
+
+for command in commands:
+    visit = [[False]*1001 for _ in range(1001)]
+    x = 500
+    y = 500
+    w = 0
+    h = 0
+    visit[x][y] = True
+    four_direction = {
+        'up':True,
+        'down':False,
+        'left':False,
+        'right':False
+    }
+
+    for d in command:
+        print(d, four_direction, x, y, w, h)
+        if four_direction['up']:
+            if d == 'F':
+                y += 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    h += 1
+            if d == 'B':
+                y -= 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    h += 1
+            if d == 'L':
+                four_direction['up'] = False
+                four_direction['left'] = True
+            if d == 'R':
+                four_direction['up'] = False
+                four_direction['right'] = True
+        elif four_direction['down']:
+            if d == 'F':
+                y -= 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    h += 1
+            if d == 'B':
+                y += 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    h += 1
+            if d == 'L':
+                four_direction['down'] = False
+                four_direction['right'] = True
+            if d == 'R':
+                four_direction['down'] = False
+                four_direction['left'] = True
+        elif four_direction['left']:
+            if d == 'F':
+                x -= 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    w += 1
+            if d == 'B':
+                x += 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    w += 1
+            if d == 'L':
+                four_direction['left'] = False
+                four_direction['down'] = True
+            if d == 'R':
+                four_direction['left'] = False
+                four_direction['up'] = True
+        elif four_direction['right']:
+            if d == 'F':
+                x += 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    w += 1
+            if d == 'B':
+                x -= 1
+                if not visit[x][y]:
+                    visit[x][y] = True
+                    w += 1
+            if d == 'L':
+                four_direction['right'] = False
+                four_direction['up'] = True
+            if d == 'R':
+                four_direction['right'] = False
+                four_direction['down'] = True
+
+    answer.append(w*h)
+
+print(*answer, sep='\n')
