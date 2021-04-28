a6ec00c76e9d1588b8e631baec588ad7f360dacf
whdals6831 <whdals6831@gmail.com>
2021-04-28 23:00:15 +0900

    update 124 나라의 숫자

--- Programmers/javascript/2_124_나라의_숫자.js
+++ Programmers/javascript/2_124_나라의_숫자.js
@@ -6,7 +6,7 @@ function solution(n) {
         n = parseInt(n/3);
 
         if (reminder == 0) {
-            n -= 1; // 정확히 나누어 떨어지며 몫이 1이 생기기때문에 -1을 해줘야함
+            n -= 1; // 정확히 나누어 떨어지면 몫이 1이 생기기때문에 -1을 해줘야함
             reminder = 4;
         }
 
