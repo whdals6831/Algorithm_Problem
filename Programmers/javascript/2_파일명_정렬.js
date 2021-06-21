1adcfa453a56060f3abf425285a52c28b38aa256
whdals6831 <whdals6831@gmail.com>
2021-06-21 23:32:24 +0900

    update 파일명 정렬

--- Programmers/javascript/2_파일명_정렬.js
+++ Programmers/javascript/2_파일명_정렬.js
@@ -1,8 +1,8 @@
 function solution(files) {
     files.sort((a,b) => {
-        var a_head = a.replace(/[^a-zA-Z]/g, " ").split(/\s+/g);
+        var a_head = a.match(/[a-zA-Z- ]+/g); // 숫자가 아닌 곳까지
         var a_number = a.match(/\d+/g);
-        var b_head = b.replace(/[^a-zA-Z]/g, " ").split(/\s+/g);
+        var b_head = b.match(/[a-zA-Z- ]+/g);
         var b_number = b.match(/\d+/g);
 
         if (a_head[0].toLowerCase() < b_head[0].toLowerCase()) return -1;
@@ -17,4 +17,4 @@ function solution(files) {
 // console.log(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]));
 // console.log(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]));
 // console.log(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]));
-console.log(solution(["fe"]));
+console.log(solution(["ffjkl-d kj595"]));
