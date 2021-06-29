986674f0fa512698f8ae6cc1bcb51e9202fe50b4
whdals6831 <whdals6831@gmail.com>
2021-06-29 21:21:23 +0900

    update 거스름돈

--- Programmers/javascript/3_거스름돈.js
+++ Programmers/javascript/3_거스름돈.js
@@ -5,7 +5,7 @@ function solution(n, money) {
     for (let i=0; i<money.length; i++) {
         for (let j=0; j<n+1; j++) {
             if (j >= money[i]) {
-                dp[j] += dp[j-money[i]];
+                dp[j] += dp[j-money[i]]; // 현재의 거스름돈을 제외한 이전의 거스름돈 dp에서의 경우의 수를 더해줌
             }
             // console.log(i, j, dp);
         }
